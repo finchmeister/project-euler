c76dc1cf7ee004e37f26fc33cc537e192fcf0684
jfinch <thefinchmeister@gmail.com>
2018-05-15 12:00:48 +0100

    5

--- 6.py
+++ 6.py
@@ -9,3 +9,24 @@ Hence the difference between the sum of the squares of the first ten natural num
 
 Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 """
+
+
+def sum_of_squares(n):
+    x = 0
+    for i in xrange(n+1):
+        x += i**2
+    return x
+
+
+def square_of_sum(n):
+    x = 0
+    for i in xrange(n + 1):
+        x += i
+    return x**2
+
+
+def difference_between_square_of_sum_and_sum_of_squares(n):
+    return square_of_sum(n) - sum_of_squares(n)
+
+
+print difference_between_square_of_sum_and_sum_of_squares(100)
