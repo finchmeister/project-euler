5668c73c1d250ed0cd8e2c7e81dca379c8f140a2
jfinch <thefinchmeister@gmail.com>
2018-05-24 14:10:16 +0100

    Wip 15

--- 15.py
+++ 15.py
@@ -10,8 +10,30 @@ class LatticePaths:
 
     def __init__(self, grid_size):
         self.grid_size = grid_size
+        self.no_of_paths = 0
+
+    def get_next_available_points(self, point):
+        available_points = []
+        across_point = (point[0] + 1, point[1])
+        if across_point[0] <= self.grid_size:
+            available_points.append(across_point)
+        down_point = (point[0], point[1] + 1)
+        if down_point[1] <= self.grid_size and down_point[1] <= down_point[0]:
+            available_points.append(down_point)
+        return available_points
+
+    # WIP
+    def find_no_of_paths(self, point=(0, 0)):
+        while point:
+            points = self.get_next_available_points(point)
+            self.no_of_paths += len(points)
+            for point in points:
+                self.find_no_of_paths(point)
+            else:
+                return
 
 
+lp = LatticePaths(20)
 
 
-lp = LatticePaths(2)
+print lp.find_no_of_paths()
