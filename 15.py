07282e3b5152909d8aac2c24755065f7cf23071e
jfinch <thefinchmeister@gmail.com>
2018-05-24 14:26:03 +0100

    Wip 15

--- 15.py
+++ 15.py
@@ -10,7 +10,7 @@ class LatticePaths:
 
     def __init__(self, grid_size):
         self.grid_size = grid_size
-        self.no_of_paths = 0
+        self.no_of_paths = 1
 
     def get_next_available_points(self, point):
         available_points = []
@@ -23,17 +23,21 @@ class LatticePaths:
         return available_points
 
     # WIP
-    def find_no_of_paths(self, point=(0, 0)):
-        while point:
-            points = self.get_next_available_points(point)
-            self.no_of_paths += len(points)
-            for point in points:
-                self.find_no_of_paths(point)
-            else:
-                return
+    def compute_no_of_paths(self, point=(0, 0)):
+        points = self.get_next_available_points(point)
+        if len(points) > 1: # As there isn't a decision in this context, we don't have more routes
+            self.no_of_paths += 1
+        for point in points:
+            self.compute_no_of_paths(point)
+        else:
+            return
+
+    def get_no_of_paths(self):
+        self.compute_no_of_paths()
+        return self.no_of_paths * 2
 
 
-lp = LatticePaths(20)
+lp = LatticePaths(2)
 
-
-print lp.find_no_of_paths()
+# Not correct
+print(lp.get_no_of_paths())
